943708427e440417a55985a5e3cdd2a882d5467c
Jorge Washington <jorgewashington92@gmail.com>
2017-12-08 23:43:47 -0200

    Event Binding

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  valorAtual: string = '';
+
+
+botaoClicado(){
+ alert('Botão clicado!!');
+}
+
+
+onKeyUp(evento: KeyboardEvent){
+  
+  this.valorAtual = (<HTMLInputElement>evento.target).value;
+  
+salvarValor(){
+
+}
+}
 }
